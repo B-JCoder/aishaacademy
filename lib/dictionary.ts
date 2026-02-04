a8f9325989548f5fd6a51ff4bbb4acc8557f0ca1
B-JCoder <bilalcodes777@gmail.com>
2026-02-05 02:05:19 +0500

    e

--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -5,5 +5,15 @@ const dictionaries = {
   es: () => import("../dictionaries/es.json").then((module) => module.default),
 };
 
-export const getDictionary = async (locale: "en" | "es") =>
-  dictionaries[locale]();
+export const getDictionary = async (locale: string) => {
+  const loadDictionary = dictionaries[locale as keyof typeof dictionaries];
+
+  if (typeof loadDictionary !== "function") {
+    console.warn(
+      `No dictionary found for locale: "${locale}". Falling back to "es".`,
+    );
+    return dictionaries.es();
+  }
+
+  return loadDictionary();
+};
